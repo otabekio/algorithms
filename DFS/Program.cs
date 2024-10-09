3a41bd4c8343d7b34b3e4b1144a97d9928b278fd
Otabek Kholmirzaev <kholmirzaevotabek1@gmail.com>
2024-10-09 16:24:45 +0500

    Add DFS

---
+++ DFS/Program.cs
@@ -0,0 +1,24 @@
+﻿Dictionary<string, List<string>> graph = new()
+{
+    { "A", new List<string>() {"C", "E"} },
+    { "B", new List<string>() {"C", "F"} },
+    { "C", new List<string>() {"A", "B", "D", "F"} },
+    { "D", new List<string>() {"C", "F"} },
+    { "E", new List<string>() {"A", "F"} },
+    { "F", new List<string>() {"B", "C", "D", "E"}},
+};
+
+Stack<string> st = new();
+st.Push("A");
+HashSet<string> visited = new();
+visited.Add("A");
+while (st.Count > 0) {
+    var curr = st.Pop();
+    Console.Write(curr + " -> ");
+    foreach (var neighbor in graph[curr]) {
+        if (!visited.Contains(neighbor)) {
+            st.Push(neighbor);
+            visited.Add(neighbor);
+        }
+    }
+}
