64839ed866da1af86902cca333dce0369d37a161
Otabek Kholmirzaev <kholmirzaevotabek1@gmail.com>
2024-10-16 21:45:30 +0500

    Union Find v2

--- UnionFind/UnionFind.cs
+++ UnionFind/UnionFind.cs
@@ -1,68 +1,44 @@
-public class UnionFind
+public class UnionFind 
 {
-    private int[] parent; // Parent array to store the root of each node
-    private int[] rank;   // Rank array to store the depth of trees
-    private int count;    // To keep track of the number of disjoint sets (islands, components)
+    int[] parent;
+    int[] size;
 
-    // Constructor to initialize the Union-Find structure
-    public UnionFind(int n)
+    public UnionFind(int n) 
     {
         parent = new int[n];
-        rank = new int[n];
-        count = n; // Initially, there are 'n' disjoint sets
-        
-        // Each node is its own parent at the start, and the rank is 0
-        for (int i = 0; i < n; i++)
+        size = new int[n];
+
+        for (int i = 0; i < n; i++) 
         {
             parent[i] = i;
-            rank[i] = 0;
+            size[i] = 1;
         }
     }
 
-    // Find the root of the element 'x', with path compression
-    public int Find(int x)
+    public int Find(int x) 
     {
-        if (parent[x] != x)
-        {
-            parent[x] = Find(parent[x]); // Path compression: make x directly point to the root
-        }
-        return parent[x];
+        while (parent[x] != x) x = parent[x];
+        return x;
     }
 
-    // Union two sets containing 'x' and 'y', return true if they were disjoint
-    public bool Union(int x, int y)
+    public bool Union(int x, int y) 
     {
-        int rootX = Find(x);
-        int rootY = Find(y);
+        var rootX = Find(x);
+        var rootY = Find(y);
 
-        // If they are already in the same set, no need to union
-        if (rootX == rootY)
-        {
-            return false;
-        }
+        if (rootX == rootY) return false;
 
-        // Union by rank: attach the shorter tree under the root of the taller tree
-        if (rank[rootX] > rank[rootY])
+        if (size[rootX] >= size[rootY]) 
         {
             parent[rootY] = rootX;
+            size[rootX] += size[rootY];
         }
-        else if (rank[rootX] < rank[rootY])
+        else 
         {
             parent[rootX] = rootY;
-        }
-        else
-        {
-            parent[rootY] = rootX;
-            rank[rootX]++; // Only increment rank when the ranks are equal
+            size[rootY] += size[rootX];
         }
 
-        count--; // Since we merged two sets, we decrease the count of disjoint sets
         return true;
-    }
-
-    // Return the number of disjoint sets (useful to count the number of connected components)
-    public int GetCount()
-    {
-        return count;
     }
 }
